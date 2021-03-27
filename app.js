5a922b4709cfc4c412ee28b32dc7bd004e0faea2
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-03-27 20:27:57 +0200

    Making a function to remove the continent name from the timezone

--- app.js
+++ app.js
@@ -11,20 +11,31 @@ const weatherDesc = document.getElementById("today-weather-desc")
 
 //Getting the current location coordinates
 navigator.geolocation.getCurrentPosition(function(position) {
-    let lat = position.coords.latitude; //current location latitude
-    let long = position.coords.longitude; //current location longitude
-    //calling the (One call API) from Open weather map
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
+        let lat = position.coords.latitude; //current location latitude
+        let long = position.coords.longitude; //current location longitude
+        //calling the (One call API) from Open weather map
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
 
-            //write the timezone name
-            let timezone = data["timezone"]
-            locationName.textContent = remContinentName(timezone)
-                //write the max temp
-            maxTemp.textContent = Math.round(data["daily"][0]["temp"]["day"] - 273.15)
-                //write the min temp
-            minTemp.textContent = Math.round(data["daily"][0]["temp"]["night"] - 273.15)
-        })
-})
+                //write the timezone name
+                let timezone = data["timezone"]
+                locationName.textContent = remContinentName(timezone)
+                    //write the max temp
+                maxTemp.textContent = Math.round(data["daily"][0]["temp"]["day"] - 273.15)
+                    //write the min temp
+                minTemp.textContent = Math.round(data["daily"][0]["temp"]["night"] - 273.15)
+
+
+            })
+    })
+    //Functions
+    //Removing the continent name from the timezone in the api to get the city name only
+let remContinentName = (timezone) => {
+    for (let x = 0; x < timezone.length; x++) {
+        if (timezone[x] == "/") {
+            return timezone.substring(x + 1)
+        }
+    }
+}
