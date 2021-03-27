9b037078250731f5b5d77b89a47405fdf3f17a84
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-03-27 18:08:22 +0200

    Declaring the main DOM elements

---
+++ app.js
@@ -0,0 +1,10 @@
+//Open Weather map API key
+const apiKey = "c1175134deb4968fd0eaf3b28c9d9ed2"
+
+//HTML elements
+const locationName = document.getElementById("location-name")
+const currentTemp = document.getElementById("current-temp")
+const maxTemp = document.getElementById("max-temp")
+const minTemp = document.getElementById("min-temp")
+const weatherIcon = document.getElementById("today-weather-icon")
+const weatherDesc = document.getElementById("today-weather-desc")
