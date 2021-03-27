c75c7e3d3fa04d23b3ec6f70b50b881b330ce9a7
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-03-27 20:31:30 +0200

    Calling (Current Weather Data) API to get the current temp, weather icon and weather description

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ navigator.geolocation.getCurrentPosition(function(position) {
                     //write the min temp
                 minTemp.textContent = Math.round(data["daily"][0]["temp"]["night"] - 273.15)
 
+                //Calling (Current Weather Data) API from OpenWeatherMap to get the current temp, weather icon and weather description
+                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${remContinentName(timezone)}&appid=${apiKey}`)
+                    .then((cityResponse) => cityResponse.json())
+                    .then((cityData) => {
+                        console.log(cityData)
+
+                    })
 
             })
     })
