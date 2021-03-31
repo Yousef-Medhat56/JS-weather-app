37167f7d6f82fc1faebd24135e19a315db51115e
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-03-31 19:56:29 +0200

    collect all the weekdays dates in one function

--- app.js
+++ app.js
@@ -42,6 +42,9 @@ navigator.geolocation.getCurrentPosition(function(position) {
                 //get weekdays names
                 getDaysName(data)
 
+                //get weekday date in the week forecast
+                getDaysDate(data)
+
                 //write the timezone name
                 locationName.textContent = remContinentName(data["timezone"])
 
@@ -53,7 +56,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
 
                         //Get the current weather icon
                         weatherIcon.innerHTML = `<img  
-                        src="//openweathermap.org/img/wn/${cityData["weather"][0]["icon"]}@2x.png">`
+                        src="https://openweathermap.org/img/wn/${cityData["weather"][0]["icon"]}@2x.png">`
 
                         //write current weather description
                         weatherDesc.textContent = cityData["weather"][0]["description"];
@@ -96,7 +99,7 @@ let createWeekDays = (halfWeekOrder, numOfDays) => {
         <div class="weekday-weather-icon"></div>
         <div class="weekday-weather-info">
             <span class="weekday-name"></span>
-            <span class="weekday-date"></span>
+            <span class="weekday-date"><span class= "weekday-date-num"></span><span class= "weekday-date-month"></span></span>
             <div class="weekday-max-min-temp"><span class ="max-temp-span"></span>/<span class ="min-temp-span"></span><span>&#8451;</span></div>
         </div>`
     }
@@ -110,7 +113,7 @@ function getWeatherIcons(weekWeatherApi) {
     let weatherIcons = document.getElementsByClassName("weekday-weather-icon")
     for (let x = 0; x < weatherIcons.length; x++) {
         weatherIcons[x].innerHTML = `<img  
-        src="//openweathermap.org/img/wn/${weekWeatherApi["daily"][x+1]["weather"][0]["icon"]}@2x.png">`
+        src="https://openweathermap.org/img/wn/${weekWeatherApi["daily"][x+1]["weather"][0]["icon"]}@2x.png">`
     }
 }
 
@@ -121,14 +124,31 @@ function getMaxMinTemp(tempType, time, weekWeatherApi) {
     }
 }
 
-//get weekdays name
+//write weekdays name
 function getDaysName(weekWeatherApi) {
-    let weekdaysOrder = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let weekdaysName = document.getElementsByClassName("weekday-name")
+    let weekdaysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let weekdaysNamesSpans = document.getElementsByClassName("weekday-name")
 
-    let milliseconds;
-    for (let x = 0; x < weekdaysName.length; x++) {
-        milliseconds = (weekWeatherApi["daily"][x]["dt"] * 1000) //convert seconds to milliseconds
-        weekdaysName[x].textContent = weekdaysOrder[new Date(milliseconds).getDay()]
+    for (let x = 0; x < weekdaysNamesSpans.length; x++) {
+        weekdaysNamesSpans[x].textContent = weekdaysName[makeDateArr(weekWeatherApi)[x].getDay()]
     }
+}
+
+//write weekday date in the week forecast
+function getDaysDate(weekWeatherApi) {
+    let weekdaysDate = document.getElementsByClassName("weekday-date-num")
+    for (let x = 0; x < weekdaysDate.length; x++) {
+        weekdaysDate[x].textContent = makeDateArr(weekWeatherApi)[x + 1].getDate()
+    }
+}
+
+//collect the date info of all the days in a one array
+function makeDateArr(weekWeatherApi) {
+    let milliseconds;
+    let dateArr = [] //to push the date values inside it
+    for (let x = 0; x < 8; x++) { //(8) : sum of today and the 7 days of the next week
+        milliseconds = (weekWeatherApi["daily"][x]["dt"] * 1000) //convert seconds to milliseconds
+        dateArr.push(new Date(milliseconds)) //add each day date to the array
+    }
+    return dateArr
 }
