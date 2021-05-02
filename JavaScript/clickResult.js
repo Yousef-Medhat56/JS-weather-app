fbe250432286911974cee6ffeaeb7496dbc73f16
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-05-02 16:57:37 +0200

    remove the clicked result from the available results array and add them into a new one

--- JavaScript/clickResult.js
+++ JavaScript/clickResult.js
@@ -1,6 +1,7 @@
 //html elements | the content of each single page
 let pageContent = mainContainer.innerHTML
 
+let rmResultsArr = new Array
 
 
 function clickResult(event) {
@@ -18,16 +19,31 @@ function clickResult(event) {
     //fetch the searched city API
     fetchResult(event)
 
+    //remove the clicked result from the available results array
+    rmClickedResult(event)
+
+
 }
 
 //fetch the searched city API
 function fetchResult(event) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${citiesArr[resultContainer.indexOf(event.target.innerHTML)]},${statesArr[resultContainer.indexOf(event.target.innerHTML)]},
-    ${countriesArr[resultContainer.indexOf(event.target.innerHTML)]}&appid=${apiKey}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${citiesArr[availableResults.indexOf(event.target.innerHTML)]},${statesArr[availableResults.indexOf(event.target.innerHTML)]},
+    ${countriesArr[availableResults.indexOf(event.target.innerHTML)]}&appid=${apiKey}&units=metric`)
         .then(result => result.json())
         .then(data => {
-            console.log(data)
-            collectWeatherData(data)
 
+            collectWeatherData(data)
         })
+}
+
+//remove the clicked results from the results array and add them to the rmResultsArr Arr
+function rmClickedResult(event) {
+    //push the clicked results to a new array
+    rmResultsArr.push(availableResults[availableResults.indexOf(event.target.innerHTML)])
+
+    citiesArr.splice(availableResults.indexOf(event.target.innerHTML), 1) //remove the clicked result from city array
+    countriesArr.splice(availableResults.indexOf(event.target.innerHTML), 1) //remove the clicked result from countries array
+    statesArr.splice(availableResults.indexOf(event.target.innerHTML), 1) //remove the clicked result from states array
+    availableResults.splice(availableResults.indexOf(event.target.innerHTML), 1) //remove the clicked result from the available results array
+
 }
