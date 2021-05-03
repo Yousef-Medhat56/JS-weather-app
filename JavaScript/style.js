05f7e3969896b597db9a1921094777a72298ddaf
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-05-03 16:40:53 +0200

    show buttons when a new page is added

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -1,5 +1,10 @@
 //the styles added by JS
 
+
+let rightBtn //right buttons
+let leftBtn //left buttons
+let pagesLength //number of pages
+
 /*keep the hover effect on the search bar and the search icon, although the user removed the cursor out of them 
 by changing their class names and add the hover styles to these new classes*/
 let keepHoverEffect = index => { //index :page index
@@ -13,4 +18,34 @@ let rmHoverEffect = index => { //index :page index
     document.querySelectorAll(".search-container-div div div")[index].classList = "search-icon"
     document.querySelectorAll(".search-container-div div input")[index].classList = "searchbar-input"
 
+}
+
+//show right and left buttons
+let showBtn = () => {
+    //get all right buttons
+    rightBtn = document.querySelectorAll(".right-btn")
+
+    //get all left buttons
+    leftBtn = document.querySelectorAll(".left-btn")
+
+    //get the number of pages
+    pagesLength = document.querySelectorAll(".container").length
+
+    //when a new page is adde (the number of pages >1)
+    if (pagesLength > 1) {
+        //show right button of the first page and hide the last one
+        rightBtn[0].style.visibility = "visible"
+        leftBtn[0].style.visibility = "hidden"
+
+        //show left button of the last page and hide the first one
+        leftBtn[pagesLength - 1].style.visibility = "visible"
+        rightBtn[pagesLength - 1].style.visibility = "hidden"
+
+
+        //show both of the right and the left buttons of the pages between the first and the last page
+        for (let x = 0; x < (pagesLength - 2); x++) {
+            rightBtn[x + 1].style.visibility = "visible"
+            leftBtn[x + 1].style.visibility = "visible"
+        }
+    }
 }
