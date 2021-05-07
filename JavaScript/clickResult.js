fd901299fb82118959ac0a5fe842b45d3f409562
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-05-07 15:46:53 +0200

    show loading spinner while loading the page

--- JavaScript/clickResult.js
+++ JavaScript/clickResult.js
@@ -1,10 +1,8 @@
-//html elements | the content of each single page
-let pageContent = mainContainer.innerHTML
-
 let rmResultsArr = new Array
 
 
 function clickResult(event) {
+
 
     //remove the hover effect on the search bar
     rmHoverEffect(searchBarIndex)
@@ -14,6 +12,8 @@ function clickResult(event) {
 
     //add new page with the searched city data
     mainContainer.innerHTML += pageContent
+
+    document.querySelector(".spinner-div").style.display = "flex"
 
     //increase the main container width 
     stretchMainCo()
@@ -34,14 +34,15 @@ function clickResult(event) {
     delBtnDisplay()
 
     //make the main container transform to the new page after adding it
-    mainContainer.style.transform = `translateX(${(document.querySelectorAll(".container").length-1)*-100}vw)`
+    mainContainer.style.transform = `translateX(${(document.querySelectorAll(".container").length - 1) * -100 }vw)`
 
 }
 
 //fetch the searched city API
 function fetchResult(event) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${citiesArr[availableResults.indexOf(event.target.innerHTML)]},${statesArr[availableResults.indexOf(event.target.innerHTML)]},
-    ${countriesArr[availableResults.indexOf(event.target.innerHTML)]}&appid=${apiKey}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${citiesArr[availableResults.indexOf(event.target.innerHTML)]},
+    ${statesArr[availableResults.indexOf(event.target.innerHTML)]},
+    ${countriesArr[availableResults.indexOf(event.target.innerHTML)] }&appid=${ apiKey }&units=metric `)
         .then(result => result.json())
         .then(data => {
 
